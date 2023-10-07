9c1f27648878f93bf08f397b07ade6862879132b
ElGibran17 <ibrahimelgibran17@gmail.com>
2023-10-07 11:22:29 +0700

    Update

---
+++ Pratikum4/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <label for="username">Username:</label>
+            <input type="text" name="username" id="username"><br>    
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"><br>
+        <button type="submit">Sumbit</button>
+    </form>
+</body>
+</html>
